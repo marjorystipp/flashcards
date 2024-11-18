53020ad93c51b6f5b2698b20e64a304cfd48ede3
marjorystipp <163565166+marjorystipp@users.noreply.github.com>
2024-11-18 13:52:25 -0300

    comentario

--- app.js
+++ app.js
@@ -2,4 +2,20 @@ function criaCartao(categoria, pergunta, resposta) {
     let container =document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+     <div class="cartao__conteudo">
+            <h3>Programação</h3>
+            <div class="cartao_conteudo_pergunta">
+                <p>  O que é JavaScript?<p> 
+            </div>
+            <div class="cartao_conteudo_resposta">
+                <p>  O CSS é uma linguagem estilização.<p> 
+            </div>
+        </div>
+    `
+    container.appendChild(cartao) 
+
+
+
 }
