fd342619f1be7a551286eadb538228fac5bf9520
marjorystipp <163565166+marjorystipp@users.noreply.github.com>
2024-11-18 13:27:46 -0300

    pergunta

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Python',
+    'O Python é uma linguagem de programação',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?,
+    'A capital da frança é Paris',
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?,
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
